0a56b3d7efe5e1bde2cba9d0ff508257eddcebfa
SaidHadad <saiddavid.hadad@gmail.com>
2021-06-15 11:54:00 -0400

    added drag and drop
    added drag and drop for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,8 +57,8 @@ $(".list-group").on("click", "p", function() {
   console.log(text);
 });
 
-// listens for when the textarea is on blur and updates the local storage with the info being edited
-// recreates the P element and replaces it on the textarea
+// listens for when the textarea is on blur and updates the local storage with the info 
+// being edited recreates the P element and replaces it on the textarea
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this)
@@ -138,6 +138,57 @@ $(".list-group").on("blur", "input[type = 'text']", function () {
   $(this).replaceWith(taskSpan);
 });
 
+// sorting tasks using jqueryui
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(tempArr);
+  }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -172,6 +223,22 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// making use of thrash div to drop and delete tasks
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -183,5 +250,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
